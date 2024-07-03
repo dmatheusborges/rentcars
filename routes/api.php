16d8afe652da3a52969090230610ce768a955fe8
Matheus Borges <dev.matheusborges@gmail.com>
2024-07-03 01:40:04 -0300

    Refactor => Criada variável para agrupar as rotas e definir o prefixo.

--- routes/api.php
+++ routes/api.php
@@ -20,20 +20,53 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+$group = (object) [
+    'marcas' => [
+        'prefix' => 'brands',
+    ],
+    'modelos' => [
+        'prefix' => 'models',
+    ],
+    'carros' => [
+        'prefix' => 'cars',
+    ],
+    'fotos' => [
+        'prefix' => 'photos',
+    ],
+    'aluguel' => [
+        'prefix' => 'rentals',
+    ],
+    'usuario' => [
+        'prefix' => 'users',
+    ],
+];
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/brands', [BrandController::class, 'store']);
+Route::group($group->marcas, function () {
+    Route::post('/', [BrandController::class, 'store']);
+});
 
-Route::post('/models', [ModeloController::class, 'store']);
+Route::group($group->modelos, function () {
+    Route::post('/', [ModeloController::class, 'store']);
+});
 
-Route::get('/cars', [CarController::class, 'index']);
-Route::post('/cars', [CarController::class, 'store']);
+Route::group($group->carros, function () {
+    Route::get('/', [CarController::class, 'index']);
+    Route::post('/', [CarController::class, 'store']);
+});
 
-Route::post('/photos', [PhotoController::class, 'store']);
+Route::group($group->fotos, function () {
+    Route::post('/', [PhotoController::class, 'store']);
+});
 
-Route::post('/rentals', [RentalController::class, 'store']);
+Route::group($group->aluguel, function () {
+    Route::post('/', [RentalController::class, 'store']);
+});
 
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{user_id}/rentals', [UserController::class, 'getUserRentals']);
+Route::group($group->usuario, function () {
+    Route::post('/', [UserController::class, 'store']);
+    Route::get('/{user_id}/rentals', [UserController::class, 'getUserRentals']);
+});
